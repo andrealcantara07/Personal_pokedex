d3c838a4d6a30c2157fa3b4c3f84754961f60ded
Andre Alcantara-Williams <aalcantarawillia@nyc.yearup.org>
2018-11-16 12:37:24 -0500

    added a class

---
+++ main.js
@@ -0,0 +1,28 @@
+axios.get('http://fizal.me/pokeapi/api/v2/name/typhlosion.json')
+  .then(test);
+
+  function test(getter){
+    console.log(getter);
+  }
+
+test();
+
+let 
+
+class Trainer {
+  constructor(name){
+  this.name= name;
+
+  }
+
+}
+
+
+console.log(Trainer.typhlosion);
+let typhlosion = new Trainer('typhlosion');
+let volcanion = new Trainer('volcanion');
+let giratina = new Trainer('giratina');
+
+console.log(typhlosion);
+console.log(volcanion);
+console.log(giratina);
